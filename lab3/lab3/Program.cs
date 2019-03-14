ba17e2dbe7c4e9a2547c41fcf207af5358913f66
Skyvoin <skyvoin14@mail.ru>
2019-03-14 13:59:00 +0700

    XXX

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -4,26 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace laba2_1
+namespace lab3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите А: ");
-            int a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите B: ");
-            int b = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите действие:");
+            Console.Write("Enter A: ");
+            int a = int.Parse(Console.ReadLine());
 
-            ConsoleKeyInfo Key = Console.ReadKey();
+            Console.Write("Enter B: ");
+            int b = int.Parse(Console.ReadLine());
 
 
-
-            char selection = Key.KeyChar;
-            switch (selection)
-            {
-            }
             Console.ReadKey();
         }
     }
