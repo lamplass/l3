10ca2aa1f9cdbbd14e03356e6685e39ae595983a
Skyvoin <skyvoin14@mail.ru>
2019-03-14 13:33:54 +0700

    add menu

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -10,6 +10,14 @@ namespace lab3
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter A: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter B: ");
+            int b = int.Parse(Console.ReadLine());
+
+
+            Console.ReadKey();
         }
     }
 }
